4112c3059aa23f52e6728cbb6a4b3fb603a5da65
Kaitlin Lim <klim20@andover.edu>
2019-07-03 15:53:54 -0700

    Updated overlap.py
    Replaced a list data structure with a dictionary data structure for greater program efficiency.

--- overlap.py
+++ overlap.py
@@ -5,7 +5,6 @@ regions.
 
 import sys
 
-# global variable that keeps track of all the chrormosomes in both files
 Chromosomes = []
 
 class Exon:
@@ -16,26 +15,21 @@ class Exon:
 
     Attributes
     ----------
-    chromosome : int
-        The number chromosome that the coding region is on.
     start : int
         The starting position of the coding region.
     end : int
         The ending position of the coding region.
     """
 
-    def __init__(self, chromosome, start, end):
+    def __init__(self, start, end):
         """
         Parameters
         ----------
-        chromosome : int
-            The number chromosome that the coding region is on.
         start : int
             The starting position of the coding region.
         end : int
             The ending position of the coding region.
         """
-        self.chromosome = chromosome
         self.start = start
         self.end = end
 
@@ -53,21 +47,26 @@ def format_file(file):
 
     Returns
     -------
-    list
-        A list of Exon objects that describe all the data points
-        in the file.
+    dictionary
+        A dictionary of Exon objects (key being the chromosome) that describe
+        all the data points in the file.
     """
 
-    exons = []
+    exons = {}
 
     with open(file, 'r') as f:
         lines = f.read().splitlines()
     # go line by line and save the elements into an Exon object
     for line in lines:
         words = line.split()
-        exons.append(Exon(int(words[0]), int(words[1]), int(words[2])))
-        if int(words[0]) not in Chromosomes:
-            Chromosomes.append(int(words[0]))
+        chromosome = words[0]
+        begin = int(words[1])
+        end = int(words[2])
+        if chromosome not in exons:
+            exons[chromosome] = []
+        exons[chromosome].append(Exon(begin, end))
+        if chromosome not in Chromosomes:
+            Chromosomes.append(chromosome)
     return exons
 
 # find the total number of overlaps
@@ -78,11 +77,11 @@ def find_overlap(exons1, exons2):
 
     Parameters
     ----------
-    exons1 : list
-        List of the all the coding regions in one file. All elements
+    exons1 : dictionary
+        Dictionary of the all the coding regions in one file. All elements
         of the list are Exon objects.
-    exons2 : list
-        List of all the coding regions in the second file. All elements
+    exons2 : dictionary
+        Dictionary of all the coding regions in the second file. All elements
         of the list are Exon objects.
 
     Returns
@@ -96,16 +95,14 @@ def find_overlap(exons1, exons2):
     for i in Chromosomes:
 
         # get all the different exons for that specific chromosome
-        subset1 = [exon for exon in exons1 if exon.chromosome == i]
-        subset2 = [exon for exon in exons2 if exon.chromosome == i]
+        if i not in exons1 or i not in exons2:
+            continue
+        subset1 = exons1[i]
+        subset2 = exons2[i]
 
         # print(subset1, subset2)
 
-        # make sure the lists are not empty
-        if not subset1 or not subset2:
-            continue
-
-       # subsets sorted in ascending order based the starting coordinate
+        # subsets sorted in ascending order based the starting coordinate
         subset1 = sorted(subset1, key=lambda x:x.start)
         subset2 = sorted(subset2, key=lambda x:x.start)
 
